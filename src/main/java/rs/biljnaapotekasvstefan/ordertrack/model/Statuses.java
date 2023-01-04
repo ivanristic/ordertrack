1b8cd8363a17567209bdd6dc654c78a5a7253adf
ivanri <ivanri@telekom.rs>
2023-01-04 15:20:34 +0100

    model change to support email send for multiple users

---
+++ src/main/java/rs/biljnaapotekasvstefan/ordertrack/model/Statuses.java
@@ -0,0 +1,35 @@
+package rs.biljnaapotekasvstefan.ordertrack.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(schema="ord", name="statuses")
+@Getter
+@Setter
+public class Statuses {
+
+    @Id
+    @Column(unique = true, updatable = false)
+    private Long statusId;
+
+    @Column
+    private String status;
+
+    @Column
+    private Boolean track;
+
+    @Column
+    private Boolean delivered;
+
+    @Column
+    private Long timeDelay;
+
+    @OneToMany(mappedBy = "statuses")
+    private Set<OrdersStatuses> ordersStatuses;
+}
---
+++ src/main/java/rs/biljnaapotekasvstefan/ordertrack/repository/StatusesRepository.java
@@ -0,0 +1,10 @@
+package rs.biljnaapotekasvstefan.ordertrack.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import rs.biljnaapotekasvstefan.ordertrack.model.Statuses;
+
+@Repository
+public interface StatusesRepository extends CrudRepository<Statuses, Long> {
+
+}
